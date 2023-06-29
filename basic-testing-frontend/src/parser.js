ec4b8663106dd0ddd458f7f7269be5f342045205
Johnny Dutra <johnny.dutra@gmail.com>
2023-06-29 17:58:12 -0300

    refactored code for lecture 38

--- basic-testing-frontend/src/parser.js
+++ basic-testing-frontend/src/parser.js
@@ -5,3 +5,8 @@ export function extractNumbers(formData) {
   return [num1Input, num2Input];
 }
 
+export function extractEnteredNumbersValues(form) {
+  const formData = new FormData(form);
+  const numberInputs = extractNumbers(formData);
+  return numberInputs;
+}
